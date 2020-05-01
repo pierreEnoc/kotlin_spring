94744b28e2acdf2f8a3566451be908bd8c03dc89
pierreEnoc <pierre_enoc@yahoo.fr>
2020-05-01 19:26:02 -0300

    creat lancamento controller

--- src/main/kotlin/com/enocp/pontointeligente/PontointeligenteApplication.kt
+++ src/main/kotlin/com/enocp/pontointeligente/PontointeligenteApplication.kt
@@ -5,6 +5,7 @@ import com.enocp.pontointeligente.documents.Funcionario
 import com.enocp.pontointeligente.enums.PerfilEnum
 import com.enocp.pontointeligente.repositories.EmpresaRepository
 import com.enocp.pontointeligente.repositories.FuncionarioRepository
+import com.enocp.pontointeligente.repositories.LancamentoRepository
 import com.enocp.pontointeligente.utils.SenhaUtils
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -12,31 +13,34 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class PontointeligenteApplication(val empresaRepository: EmpresaRepository,
-																	val funcionarioRepository: FuncionarioRepository) : CommandLineRunner {
-	override fun run (vararg args: String?){
+																	val funcionarioRepository: FuncionarioRepository,
+																	val lancamentoRepository: LancamentoRepository): CommandLineRunner {
+	
+	override fun run(vararg args: String?){
 		empresaRepository.deleteAll()
 		funcionarioRepository.deleteAll()
+		lancamentoRepository.deleteAll()
 		
-	val empresa: Empresa = Empresa("Empresa", "10443887000146")
-		empresaRepository.save(empresa)
+		var empresa: Empresa = Empresa("RazaoSocial", "71593640000109")
+		empresa = empresaRepository.save(empresa)
 		
-		val admin: Funcionario = Funcionario("admin","admin@empresa.com",
-			SenhaUtils().gerarBCrypt("123456"), "25708317000",
+		var admin: Funcionario = Funcionario("Admin", "admin@teste.com.br", SenhaUtils().gerarBcrypt("123456"), "12920460702",
 			PerfilEnum.ROLE_ADMIN, empresa.id!!)
-		funcionarioRepository.save(admin)
+		admin = funcionarioRepository.save(admin)
 		
-		val funcinario: Funcionario= Funcionario("Funcionario",
-			"funcinario@empresa.com", SenhaUtils().gerarBCrypt("123456"),
-			"44325441557", PerfilEnum.ROLE_USUARIO, empresa.id!!)
-		funcionarioRepository.save(funcinario)
+		var funcionario: Funcionario = Funcionario("Usuario", "usuario@teste.com.br", SenhaUtils().gerarBcrypt("123456"), "12920460702",
+			PerfilEnum.ROLE_USUARIO, empresa.id!!)
+		funcionario = funcionarioRepository.save(funcionario)
 		
-		System.out.println("Empresa ID: " + empresa.id)
-		System.out.println("Admin ID: " + admin.id)
-		System.out.println("Funcionario ID " + funcinario.id)
+		System.out.println("Empresa ID: "+ empresa.id)
+		System.out.println("Admin ID: "+ admin.id)
+		System.out.println("Funcionario ID: "+ funcionario.id)
+		
 	}
-	
 }
 
+
+//gerarBCrypt
 	fun main(args: Array<String>) {
 	runApplication<PontointeligenteApplication>(*args)
 }
